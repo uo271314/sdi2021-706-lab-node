f47a5f76a8ee241ef988b647393daacf217a2cd3
Laura <uo271314@uniovi.es>
2021-04-14 13:13:40 +0200

    SDI-2021-706-9.4-Sistema de compras.

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ routerUsuarioSession.use(function(req, res, next) {
 //Aplicar routerUsuarioSession
 app.use("/canciones/agregar",routerUsuarioSession);
 app.use("/publicaciones",routerUsuarioSession);
+app.use("/cancion/comprar",routerUsuarioSession);
+app.use("/compras",routerUsuarioSession);
 
 //routerUsuarioAutor
 let routerUsuarioAutor = express.Router();
@@ -74,7 +76,19 @@ routerAudios.use(function(req, res, next) {
           if(req.session.usuario && canciones[0].autor == req.session.usuario ){
              next();
           } else {
-             res.redirect("/tienda");
+              let criterio = {
+                  usuario : req.session.usuario,
+                  cancionId : mongo.ObjectID(idCancion)
+              };
+
+              gestorBD.obtenerCompras(criterio ,function(compras){
+                  if (compras != null && compras.length > 0 ){
+                      next();
+                  } else {
+                      res.redirect("/tienda");
+                  }
+              });
+
           }
        })
 });
