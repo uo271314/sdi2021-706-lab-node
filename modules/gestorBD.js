b6d52d515234710ddb53c852853d3e15f568e463
Laura <uo271314@uniovi.es>
2021-04-14 12:16:08 +0200

    SDI-2021-706-9.1-Gestión de colecciones, eliminar documento

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -5,6 +5,23 @@ module.exports = {
         this.mongo = mongo;
         this.app = app;
     },
+    eliminarCancion : function(criterio, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('canciones');
+                collection.remove(criterio, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
     obtenerCanciones : function(criterio, funcionCallback){
         this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
             if (err) {
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -210,4 +210,14 @@ module.exports = function(app, swig, gestorBD, mcomentarios) {
             callback(true); // FIN
         }
     };
+    app.get('/cancion/eliminar/:id', function (req, res) {
+        let criterio = {"_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        gestorBD.eliminarCancion(criterio,function(canciones){
+            if ( canciones == null ){
+                res.send(respuesta);
+            } else {
+                res.redirect("/publicaciones");
+            }
+        });
+    })
 };
