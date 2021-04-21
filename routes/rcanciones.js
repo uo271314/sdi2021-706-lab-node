9ec92c136e77c3ce071e1e4db85dd4799610255b
Laura <uo271314@uniovi.es>
2021-04-22 01:19:41 +0200

    SDI-2021-706-10.5-SW Consumir SW desde NodeJS.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -134,13 +134,28 @@ module.exports = function(app, swig, gestorBD, mcomentarios) {
                         res.send(respuesta);
                     else{
                         usuarioPuedeComprarCancion(req.session.usuario,cancionId,function (comprarValido) {
-                            respuesta = swig.renderFile('views/bcancion.html',
-                                {
-                                    cancion : canciones[0],
-                                    comentarios : comentarios,
-                                    comprarValido : comprarValido
-                                });
-                            res.send(respuesta);
+                            let configuracion = {
+                                url: "https://www.freeforexapi.com/api/live?pairs=EURUSD",
+                                method: "get",
+                                headers: {
+                                    "token": "ejemplo",
+                                }
+                            }
+                            let rest = app.get("rest");
+                            rest(configuracion, function (error, response, body) {
+                                console.log("cod: " + response.statusCode + " Cuerpo :" + body);
+                                let objetoRespuesta = JSON.parse(body);
+                                let cambioUSD = objetoRespuesta.rates.EURUSD.rate;
+                                // nuevo campo "usd"
+                                canciones[0].usd = cambioUSD * canciones[0].precio;
+                                respuesta = swig.renderFile('views/bcancion.html',
+                                    {
+                                        cancion : canciones[0],
+                                        comentarios : comentarios,
+                                        comprarValido : comprarValido
+                                    });
+                                res.send(respuesta);
+                            })
                         });
                     }
                 });
